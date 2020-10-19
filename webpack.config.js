a899753b48521f6368830bd592059c7613d671e1
Juansito_the_dev <tecnosebastube@gmail.com>
2020-10-19 09:54:20 -0500

    empezamos con webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,32 @@
 const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.js',
+        path: path.resolve(__dirname,'dist'),
+        filename: 'main.js'
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.js']
     },
-    module: {
-        rules: [
-            {
-                test: /\.js?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                }
+    module:{
+        rules:[{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            use:{
+                loader: 'babel-loader'
             }
-        ]
+        }]
     },
     plugins: [
-        new HtmlWebpackPlugin([
+        new HtmlWebPackPlugin([
             {
                 inject: true,
                 template: './public/index.html',
-                filename: './index.html'
+                filename: './index.html',
             }
         ])
     ]
+
 }
